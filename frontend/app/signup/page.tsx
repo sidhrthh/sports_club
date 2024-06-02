402c020b30eb516c0e84d5531095e41d70c4a54c
Sidharth pandro <sidharthpandro50@gmail.com>
2024-06-02 22:14:23 +0530

    topbar

---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,10 @@
+import Topbar from "@/components/Topbar";
+
+export default function signup() {
+    return (
+        <>
+        <Topbar />
+        <h2> Hi, from the Sign up page.</h2>
+        </>
+    )
+}
---
+++ frontend/components/Topbar.tsx
@@ -0,0 +1,5 @@
+export default function Topbar() {
+    return (
+        <p className="bg-black text-white text-center p-3">T20 Sales For All The Cricket Things And Free Express Delivery - OFF 50%! <a href="#" className="underline"> Shop now</a></p>
+    )
+}
